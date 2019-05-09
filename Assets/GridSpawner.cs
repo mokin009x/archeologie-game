8d09a329f6be3021b3cbded10a9feb8f51aa1ad7
Kars van den Eijnde <aofbliz@hotmail.com>
2019-05-09 09:30:47 +0200

    Grid Spawner added

---
+++ Assets/GridSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+    public int Resolution;
+    public GameObject blockPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < Resolution; x++)
+        {
+            for (int z = 0; z < Resolution; z++)
+            {
+                GameObject block = Instantiate(blockPrefab , Vector3.zero , transform.rotation) as GameObject;
+
+                block.transform.parent = transform;
+                block.transform.localPosition = new Vector3(x,0,z);
+            }
+        }
+    }
+}
