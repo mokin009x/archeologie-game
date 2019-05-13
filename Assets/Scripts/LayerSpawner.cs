0c653edc30c8a8d82fd4c46f1c4be3908766f6db
Kars van den Eijnde <aofbliz@hotmail.com>
2019-05-13 10:28:41 +0200

    Layer Spawner Added

---
+++ Assets/Scripts/LayerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSpawner : MonoBehaviour
+{
+    public GameObject[] layers;
+    public int holeDepth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < holeDepth; i++)
+        {
+            GameObject tempLayer = Instantiate(layers[Random.Range(0, layers.Length)]);
+            tempLayer.transform.localPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+ i*0.5f, gameObject.transform.position.z);
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
