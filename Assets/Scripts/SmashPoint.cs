bc07fa10651882275bb3678e8291c427c3ac84f1
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-05-14 11:30:37 +0200

    phone input working

--- Assets/Scripts/SmashPoint.cs
+++ Assets/Scripts/SmashPoint.cs
@@ -18,6 +18,7 @@ public class SmashPoint : MonoBehaviour
         {
             Destroy(stoneLayer);
         }
+        Destroy(this);
 
     }
 }
--- Assets/Scripts/playerControlScript.cs
+++ Assets/Scripts/playerControlScript.cs
@@ -15,21 +15,21 @@ public class playerControlScript : MonoBehaviour
 
     void Awake()
     {
-        /*width = (float)Screen.width / 2.0f;
+        width = (float)Screen.width / 2.0f;
         height = (float)Screen.height / 2.0f;
 
         // Position used for the cube.
-        position = new Vector3(0.0f, 0.0f, 0.0f);*/
+        position = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     void OnGUI()
     {
-        /*// Compute a fontSize based on the size of the screen width.
+        // Compute a fontSize based on the size of the screen width.
         GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
 
         GUI.Label(new Rect(20, 20, width, height * 0.25f),
             "x = " + position.x.ToString("f2") +
-            ", y = " + position.y.ToString("f2"));*/
+            ", y = " + position.y.ToString("f2"));
     }
 
     void Update()
@@ -47,10 +47,16 @@ public class playerControlScript : MonoBehaviour
                 // Create a particle if hit
                 if (Physics.Raycast(ray, out hit, Mathf.Infinity))
                 {
-/*
-                   hit.collider.GetComponent<>()
-*/
+                    Instantiate(particle, hit.point, Quaternion.identity);
+
+                    /*if (hit.collider.gameObject.CompareTag("Smash Point"))
+                    {
+
+                    }*/
+                    hit.collider.GetComponent<SmashPoint>().DestroyThisObject();
                 }
+
+                
             }
         }
     
