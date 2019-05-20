af57c81666a7c45630d1400e81140d1a3501568b
Kars van den Eijnde <aofbliz@hotmail.com>
2019-05-20 11:55:20 +0200

    Visual Update
    Bling fixed
    new dirt

---
+++ Assets/Scripts/Artifact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artifact : MonoBehaviour
+{
+    [SerializeField]
+    bool clicked;
+    public Transform MoveHere;
+    [SerializeField]
+    float speed;
+
+    private void Start()
+    {
+        clicked = false;
+    }
+    private void FixedUpdate()
+    {
+        if (clicked)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MoveHere.position, speed * Time.deltaTime);
+        }
+    }
+    public void OnTap()
+    {
+        clicked = true;
+    }
+}
