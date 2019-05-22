be03b98b617dc89777bf8a81edcd2b7a2b6e2520
Kars van den Eijnde <aofbliz@hotmail.com>
2019-05-22 15:30:46 +0200

    Forgot to push this OOF

--- Assets/Scripts/SceneSwitchScript.cs
+++ Assets/Scripts/SceneSwitchScript.cs
@@ -5,33 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitchScript : MonoBehaviour
 {
-    public static SceneSwitchScript instance;
-
-    public void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(this);
-        }
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SwitchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
