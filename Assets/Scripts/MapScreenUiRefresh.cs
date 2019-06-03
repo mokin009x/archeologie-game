04611b96c00bf6c35650501f3d5ea19fd7f8efca
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-06-03 10:18:46 +0200

    mapscreen money ui

---
+++ Assets/Scripts/MapScreenUiRefresh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MapScreenUiRefresh : MonoBehaviour
+{
+    public GameObject money;
+    // Start is called before the first frame update
+    void Start()
+    {
+        money.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = PlayerData.instance.GetCredits().ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
