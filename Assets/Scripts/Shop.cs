197aaad6210d84ace690ef953e48092c0344f7b0
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-06-17 09:12:54 +0200

    Ui verplaatst/ vervangen

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -19,10 +19,9 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
         moneyCounter.GetComponentInChildren<TextMeshProUGUI>().text = PlayerData.instance.GetCredits().ToString();
         pikhouweelCounter.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + PlayerData.instance.GetPikhouweelLevel().ToString();
-        
+
         schepCounter.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + PlayerData.instance.GetSchepLevel().ToString();
 
         if (PlayerData.instance.GetStartPrice() < 125)
@@ -37,6 +36,7 @@ public class Shop : MonoBehaviour
             pikhouweelPrice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Prijs " + price.ToString();
             schepPrice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Prijs " + price2.ToString();
         }
+
         /*currentToolLevel = 1;
         currentCredits = 0;*/
         // dit moeten we miss uitcommenten 
@@ -45,26 +45,25 @@ public class Shop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void PikhouweelUpgrade(int price)
     {
         currentCredits = PlayerData.instance.GetCredits();
         currentPikhouweelLevel = PlayerData.instance.GetPikhouweelLevel();
-        
+
         if (currentPikhouweelLevel <= Mathf.Infinity)
         {
             if (currentCredits >= price * PlayerData.instance.GetPikhouweelLevel())
             {
                 PlayerData.instance.DecreaseCredits(price * PlayerData.instance.GetPikhouweelLevel());
-                PlayerData.instance.pikhouweelLevel ++;
+                PlayerData.instance.pikhouweelLevel++;
                 currentPikhouweelLevel = PlayerData.instance.pikhouweelLevel;
                 PlayerPrefs.SetInt("PikhouweelLevel", currentPikhouweelLevel);
                 PlayerPrefs.Save();
             }
         }
-        
+
         var newPrice = price * PlayerData.instance.GetPikhouweelLevel();
 
         ShopUiUpdate(pikhouweelCounter, currentPikhouweelLevel, newPrice, pikhouweelPrice);
@@ -74,31 +73,28 @@ public class Shop : MonoBehaviour
     {
         currentCredits = PlayerData.instance.GetCredits();
         currentSchepLevel = PlayerData.instance.GetSchepLevel();
-        
+
         if (currentSchepLevel <= Mathf.Infinity)
         {
             if (currentCredits >= price * PlayerData.instance.GetSchepLevel())
             {
                 PlayerData.instance.DecreaseCredits(price * PlayerData.instance.GetSchepLevel());
-                PlayerData.instance.schepLevel ++;
+                PlayerData.instance.schepLevel++;
                 currentSchepLevel = PlayerData.instance.schepLevel;
                 PlayerPrefs.SetInt("SchepLevel", currentSchepLevel);
                 PlayerPrefs.Save();
             }
         }
-        
+
         var newPrice = price * PlayerData.instance.GetSchepLevel();
-        
+
         ShopUiUpdate(schepCounter, currentSchepLevel, newPrice, schepPrice);
     }
-
 
     public void ShopUiUpdate(GameObject levelBox, int newToolLevel, int newPrice, GameObject priceCounter)
     {
         moneyCounter.GetComponentInChildren<TextMeshProUGUI>().text = PlayerData.instance.GetCredits().ToString();
         priceCounter.GetComponentInChildren<TextMeshProUGUI>().text = "Prijs " + newPrice.ToString();
         levelBox.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Level " + newToolLevel.ToString();
-        
     }
-
 }
