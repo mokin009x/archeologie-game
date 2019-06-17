40c81c3eaf69988907bde0cd1902416f49fdb471
Kars van den Eijnde <aofbliz@hotmail.com>
2019-06-17 09:13:25 +0200

    Level 1 Now looks alright

---
+++ Assets/Scripts/OnButtonClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnButtonClick : MonoBehaviour
+{
+    AudioSource audio;
+    public AudioClip clickSound;
+    
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    public void PlayAudio()
+    {
+        audio.PlayOneShot(clickSound);
+    }
+}
